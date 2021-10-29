0c8c5c378d3895c93001e8115418ad096e689fda
Alli Case <allicase99@gmail.com>
2021-10-29 14:41:30 -0600

    fixed broken poster image & styled title captions

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -5,7 +5,7 @@ let filmList = document.querySelector("#filmList");
 for (let i = 0; i < films.length; i++) {
   let figure = document.createElement("figure");
   let figImg = document.createElement("img");
-  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i}.jpg`;
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i+1}.jpg`;
   let figCaption = document.createElement("figcaption");
   figCaption.textContent = films[i].title
 
