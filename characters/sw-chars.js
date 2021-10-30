f3fd7c7abc6f7e32e9ba6c612a953593d032b60a
Alli Case <allicase99@gmail.com>
2021-10-30 09:30:39 -0600

    added an image for characters

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,2 +1,15 @@
 import { people } from '../data/people.js'
 
+const main = document.querySelector('#main')
+
+const personFig = document.createElement('figure')
+const personImg = document.createElement('img')
+personImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`;
+const personCaption = document.createElement('figcaption')
+personCaption.textContent = 'person name goes here'
+
+personFig.appendChild(personImg)
+personFig.appendChild(personCaption)
+
+main.appendChild(personFig)
+
