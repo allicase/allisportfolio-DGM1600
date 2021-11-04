4e0278f885178fd21dc1401da2071e602b594b26
Alli Case <allicase99@gmail.com>
2021-11-03 20:23:57 -0600

    fixed broken image

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -4,17 +4,25 @@ const main = document.querySelector("#main");
 
 console.log(people.length);
 
-people.forEach((element, index) => {
-  const personFig = document.createElement("figure");
-  const personImg = document.createElement("img");
-  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${
-    index + 1
-  }.jpg`;
-  const personCaption = document.createElement("figcaption");
-  personCaption.textContent = "person name goes here";
+people.forEach((element) => {
+  const personFig = document.createElement('figure');
+  const personImg = document.createElement('img');
+  let charNum = getLastNumber(element.url)
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+  const personCaption = document.createElement('figcaption');
+  personCaption.textContent = element.name
 
   personFig.appendChild(personImg);
   personFig.appendChild(personCaption);
 
   main.appendChild(personFig);
-});
+})
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
