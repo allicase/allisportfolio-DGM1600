687d95c1eabe57225d03bc923ee9bfb54c834820
Alli Case <allicase99@gmail.com>
2021-11-07 00:48:43 -0600

    created all 4 buttons, make the clicks get filters

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -5,19 +5,24 @@ const main = document.querySelector("#main");
 const mainHeader = document.createElement("header");
 document.body.insertBefore(mainHeader, main);
 
+const allButton = document.createElement("button");
+allButton.textContent = "All Characters";
+allButton.addEventListener("click", () => populateDOM(people));
+mainHeader.appendChild(allButton);
+
 const maleButton = document.createElement("button");
 maleButton.textContent = "Male Characters";
-
-maleButton.addEventListener("click", () => console.log('Thanks for clicking'));
-
+maleButton.addEventListener("click", () => populateDOM(maleCharacters));
 mainHeader.appendChild(maleButton);
 
 const femaleButton = document.createElement("button");
 femaleButton.textContent = "Female Characters";
+femaleButton.addEventListener("click", () => populateDOM(femaleCharacters));
 mainHeader.appendChild(femaleButton);
 
 const othersButton = document.createElement("button");
 othersButton.textContent = "Other Characters";
+othersButton.addEventListener("click", () => populateDOM(otherCharacters));
 mainHeader.appendChild(othersButton);
 
 const maleCharacters = people.filter((person) => person.gender === "male");
@@ -25,24 +30,35 @@ const maleCharacters = people.filter((person) => person.gender === "male");
 const femaleCharacters = people.filter((person) => person.gender === "female");
 
 const otherCharacters = people.filter((person) => {
-  if (person.gender === "n/a" || person.gender === "hermaphrodite") {
+  if (
+    person.gender === "n/a" ||
+    person.gender === "hermaphrodite" ||
+    person.gender === "none"
+  ) {
     return person;
   }
 });
 
-people.forEach((element) => {
-  const personFig = document.createElement("figure");
-  const personImg = document.createElement("img");
-  let charNum = getLastNumber(element.url);
-  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
-  const personCaption = document.createElement("figcaption");
-  personCaption.textContent = element.name;
+function populateDOM(characters) {
+  // remove all the previous items before populating with new ones
+  while (main.firstChild) {
+    main.removeChild(main.firstChild)
+  }
 
-  personFig.appendChild(personImg);
-  personFig.appendChild(personCaption);
+    characters.forEach((element) => {
+    const personFig = document.createElement("figure");
+    const personImg = document.createElement("img");
+    let charNum = getLastNumber(element.url);
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+    const personCaption = document.createElement("figcaption");
+    personCaption.textContent = element.name;
 
-  main.appendChild(personFig);
-});
+    personFig.appendChild(personImg);
+    personFig.appendChild(personCaption);
+
+    main.appendChild(personFig);
+  });
+}
 
 function getLastNumber(url) {
   let end = url.lastIndexOf("/");
