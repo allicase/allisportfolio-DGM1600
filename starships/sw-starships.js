26aed18abaf2db56fa93cbb4840bd7035452a632
Alli Case <allicase99@gmail.com>
2021-11-08 15:03:36 -0700

    connected images to starship list

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,4 +1,5 @@
 import { starships } from "../data/starships.js";
+import { getLastNumber } from "../utils/index.js";
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
@@ -10,9 +11,20 @@ function populateNav() {
         anchorWrap.href = '#'
         let listItem = document.createElement('li')
         listItem.textContent = starship.name
+
+        anchorWrap.addEventListener('click', () => populateShipView(starship))
+
         anchorWrap.appendChild(listItem)
         navList.appendChild(anchorWrap)
     })
 }
 
 populateNav()
+
+function populateShipView(shipData) {
+ console.log(`You clicked on ${shipData.name}`)
+ let shipImage = document.createElement('img')
+ let shipNum = getLastNumber(shipData.url)
+ shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+ shipView.appendChild(shipImage)
+}
