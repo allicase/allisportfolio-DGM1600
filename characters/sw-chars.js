0b6e118f1f427020bf43a01535c866e8a52bec71
Alli Case <allicase99@gmail.com>
2021-11-08 15:10:43 -0700

    added removeChildren

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,5 +1,5 @@
 import { people } from "../data/people.js";
-import { getLastNumber } from "../utils/index.js";
+import { getLastNumber, removeChildren } from "../utils/index.js";
 
 const main = document.querySelector("#main");
 
@@ -42,9 +42,7 @@ const otherCharacters = people.filter((person) => {
 
 function populateDOM(characters) {
   // remove all the previous items before populating with new ones
-  while (main.firstChild) {
-    main.removeChild(main.firstChild)
-  }
+  removeChildren(main)
 
     characters.forEach((element) => {
     const personFig = document.createElement("figure");
--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,30 +1,31 @@
 import { starships } from "../data/starships.js";
-import { getLastNumber } from "../utils/index.js";
+import { getLastNumber, removeChildren } from "../utils/index.js";
 
-const nav = document.querySelector('.nav')
-const navList = document.querySelector('.navList')
-const shipView = document.querySelector('.shipViewer')
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".navList");
+const shipView = document.querySelector(".shipViewer");
 
 function populateNav() {
-    starships.forEach(starship => {
-        let anchorWrap = document.createElement('a')
-        anchorWrap.href = '#'
-        let listItem = document.createElement('li')
-        listItem.textContent = starship.name
+  starships.forEach((starship) => {
+    let anchorWrap = document.createElement("a");
+    anchorWrap.href = "#";
+    let listItem = document.createElement("li");
+    listItem.textContent = starship.name;
 
-        anchorWrap.addEventListener('click', () => populateShipView(starship))
+    anchorWrap.addEventListener("click", () => populateShipView(starship));
 
-        anchorWrap.appendChild(listItem)
-        navList.appendChild(anchorWrap)
-    })
+    anchorWrap.appendChild(listItem);
+    navList.appendChild(anchorWrap);
+  });
 }
 
-populateNav()
+populateNav();
 
 function populateShipView(shipData) {
- console.log(`You clicked on ${shipData.name}`)
- let shipImage = document.createElement('img')
- let shipNum = getLastNumber(shipData.url)
- shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
- shipView.appendChild(shipImage)
+  removeChildren(shipView);
+  console.log(`You clicked on ${shipData.name}`);
+  let shipImage = document.createElement("img");
+  let shipNum = getLastNumber(shipData.url);
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  shipView.appendChild(shipImage);
 }
