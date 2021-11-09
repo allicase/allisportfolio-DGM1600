b74f33a865f19e7d1ae03c77d79b4bf09ed444f8
Alli Case <allicase99@gmail.com>
2021-11-08 22:04:46 -0700

    updated home page links

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -34,9 +34,10 @@ function populateNav() {
 populateNav();
 
 function populateShipView(shipData) {
-  removeChildren(shipView);
-  console.log(`You clicked on ${shipData.name}`);
-  let shipImage = document.createElement("img");
+  removeChildren(shipView)
+  let shipImage = document.createElement('img')
+  let shipName = document.createElement('div')
+  shipName.textContent = shipData.name
   let shipNum = getLastNumber(shipData.url);
   shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
   shipImage.addEventListener("error", () => {
@@ -45,4 +46,5 @@ function populateShipView(shipData) {
     shipMessage.textContent = `${shipData.name} is not available`;
   });
   shipView.appendChild(shipImage);
+  shipView.appendChild(shipName)
 }
