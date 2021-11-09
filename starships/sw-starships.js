3e6c897c93ae03972d827f5a819ef78c7f82fa1f
Alli Case <allicase99@gmail.com>
2021-11-08 21:23:54 -0700

    added an error modal for broken images

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -5,6 +5,18 @@ const nav = document.querySelector(".nav");
 const navList = document.querySelector(".navList");
 const shipView = document.querySelector(".shipViewer");
 
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal-close");
+const modalBackground = document.querySelector(".modal-background");
+const shipMessage = document.querySelector(".shipMessage");
+
+closeButton.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+modalBackground.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+
 function populateNav() {
   starships.forEach((starship) => {
     let anchorWrap = document.createElement("a");
@@ -27,5 +39,10 @@ function populateShipView(shipData) {
   let shipImage = document.createElement("img");
   let shipNum = getLastNumber(shipData.url);
   shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  shipImage.addEventListener("error", () => {
+    shipImage.hidden = true;
+    modal.classList.toggle("is-active");
+    shipMessage.textContent = `${shipData.name} is not available`;
+  });
   shipView.appendChild(shipImage);
 }
