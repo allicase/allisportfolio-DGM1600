85120da56ec2b12096522c38c5c67782285db721
Alli Case <allicase99@gmail.com>
2021-11-10 23:56:13 -0700

    added simplified senators and party filter

--- congress/congress.js
+++ congress/congress.js
@@ -1,18 +1,45 @@
-import { senators } from "../data/senators.js"
+import { senators } from "../data/senators.js";
 
-const senatorDiv = document.querySelector('.senators')
+const senatorDiv = document.querySelector(".senators");
 
-function populateSenatorDiv() {
-    senators.forEach(senator => {
-        let senFigure = document.createElement('figure')
-        let figImg = document.createElement('img')
-        let figCaption = document.createElement('figCaption')
-
-        figCaption.textContent = senator.first_name
-        senFigure.appendChild(figImg)
-        senFigure.appendChild(figCaption)
-        senatorDiv.appendChild(senFigure)
-    })
+function simplifiedSenators(senatorArray) {
+  return senatorArray.map((senator) => {
+    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
+    return {
+      id: senator.id,
+      name: `${senator.first_name}${middleName}${senator.last_name}`,
+      party: senator.party,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+      gender: senator.gender,
+      seniority: "later",
+      missedVotesPct: senator.missed_votes_pct,
+      loyaltyPct: senator.votes_with_party_pct,
+    };
+  });
 }
 
-populateSenatorDiv()
+populateSenatorDiv(simplifiedSenators(senators));
+
+function populateSenatorDiv(simpleSenators) {
+  simpleSenators.forEach((senator) => {
+    let senFigure = document.createElement("figure");
+    let figImg = document.createElement("img");
+    let figCaption = document.createElement("figCaption");
+
+    figImg.src = senator.imgURL;
+
+    figCaption.textContent = senator.name;
+    senFigure.appendChild(figImg);
+    senFigure.appendChild(figCaption);
+    senatorDiv.appendChild(senFigure);
+  });
+}
+
+const filterSenators = (prop, value) => {
+    return simplifiedSenators(senators).filter(senator => senator[prop] === value)
+}
+
+const republicans = filterSenators("party", "R")
+const femaleSenators = filterSenators("gender", "F")
+
+console.log(femaleSenators)
