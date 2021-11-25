892fd25f9e2a440c1c9c947b1b279b9ece543219
Alli Case <allicase99@gmail.com>
2021-11-25 00:09:02 -0700

    fixed pokemon api function

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,8 +1,16 @@
-async funciton getAPIData(url) {
-    try {
-
-    }
-    catch (error) {
-        console.error(error)
-    }
+async function getAPIData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+getAPIData("https://pokeapi.co/api/v2/pokemon/ditto")
+.then((data) => {
+    console.log(data)
+}
+
+)
