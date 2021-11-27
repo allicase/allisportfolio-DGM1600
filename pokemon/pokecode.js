707fba3a5fc2a3640f5774abad38c34ab5305822
Alli Case <allicase99@gmail.com>
2021-11-26 22:02:50 -0700

    added populatePokeCards function

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -11,6 +11,23 @@ async function getAPIData(url) {
 getAPIData("https://pokeapi.co/api/v2/pokemon/ditto")
 .then((data) => {
     console.log(data)
-}
+    populatePokeCards(data)
+})
 
-)
+const pokeGrid = document.querySelector('.pokeGrid')
+
+function populatePokeCards(singlePokemon) {
+  const pokeScene = document.createElement('div')
+  pokeScene.className = "scene"
+  const pokeCard = document.createElement('div')
+  pokeCard.className = "card"
+  const pokeFront= document.createElement('div')
+  pokeFront.className = "cardFace front"
+  const pokeBack = document.createElement('div')
+  pokeBack.className = "cardFace back"
+
+  pokeCard.appendChild(pokeFront)
+  pokeCard.appendChild(pokeBack)
+  pokeScene.appendChild(pokeCard)
+  pokeGrid.appendChild(pokeScene)
+}
